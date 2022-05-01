63feede86c7e32d1238c0fedb3b6250cb69ee450
nigelwhite <webdev@permaculture.org.uk>
2022-05-01 08:06:07 +0100

    images generate

--- .eleventy.js
+++ .eleventy.js
@@ -4,7 +4,29 @@ const syntaxHighlight = require('@11ty/eleventy-plugin-syntaxhighlight');
 const htmlmin = require('html-minifier');
 const Image = require('@11ty/eleventy-img');
 
+async function imageShortcode(src, alt, sizes) {
+	let metadata = await Image(src, {
+		widths: [600, 900, 1500],
+		formats: ['avif', 'webp', 'jpeg'],
+		urlPath: '/images/',
+		outputDir: './_site/images/',
+	});
+
+	let imageAttributes = {
+		alt,
+		sizes,
+		loading: 'lazy',
+		decoding: 'async',
+	};
+
+	// You bet we throw an error on missing alt in `imageAttributes` (alt="" works okay)
+	return Image.generateHTML(metadata, imageAttributes);
+}
+
 module.exports = function(eleventyConfig) {
+	// generate images
+	eleventyConfig.addNunjucksAsyncShortcode('image', imageShortcode);
+
 	// Disable automatic use of your .gitignore
 	eleventyConfig.setUseGitIgnore(false);
 

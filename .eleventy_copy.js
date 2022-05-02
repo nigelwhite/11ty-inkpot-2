bd612f640450ec7b01e3514de03160c40debdc03
nigelwhite <webdev@permaculture.org.uk>
2022-05-02 08:49:46 +0100

    picture displays ok with zac version of code

--- .eleventy_copy.js
+++ .eleventy_copy.js
@@ -5,35 +5,51 @@ const htmlmin = require('html-minifier');
 const Image = require('@11ty/eleventy-img');
 const path = require('path');
 
-async function imageShortcode(src, alt, sizes = "100vw") {
-	if(alt === undefined) {
-	  // You bet we throw an error on missing alt (alt="" works okay)
-	  throw new Error(`Missing \`alt\` on responsiveimage from: ${src}`);
+async function imageShortcode(src, alt) {
+	let sizes = '100vw';
+	let srcPrefix = `./src/static/images/`;
+	src = srcPrefix + src;
+	console.log(`Generating image(s) from:  ${src}`);
+	if (alt === undefined) {
+		// Throw an error on missing alt (alt="" works okay)
+		throw new Error(`Missing \`alt\` on responsiveimage from: ${src}`);
 	}
-  
 	let metadata = await Image(src, {
 		widths: [600, 900, 1500],
 		formats: ['avif', 'webp', 'jpeg'],
 		urlPath: '/images/',
 		outputDir: './_site/images/',
+		/* =====
+	  Now we'll make sure each resulting file's name will 
+	  make sense to you. **This** is why you need 
+	  that `path` statement mentioned earlier.
+	  ===== */
+		filenameFormat: function(id, src, width, format, options) {
+			const extension = path.extname(src);
+			const name = path.basename(src, extension);
+			return `${name}-${width}w.${format}`;
+		},
 	});
-  
 	let lowsrc = metadata.jpeg[0];
-	let highsrc = metadata.jpeg[metadata.jpeg.length - 1];
-  
 	return `<picture>
-	  ${Object.values(metadata).map(imageFormat => {
-		return `  <source type="${imageFormat[0].sourceType}" srcset="${imageFormat.map(entry => entry.srcset).join(", ")}" sizes="${sizes}">`;
-	  }).join("\n")}
-		<img
-		  src="${lowsrc.url}"
-		  width="${highsrc.width}"
-		  height="${highsrc.height}"
-		  alt="${alt}"
-		  loading="lazy"
-		  decoding="async">
-	  </picture>`;
-  }
+	  ${Object.values(metadata)
+			.map((imageFormat) => {
+				return `  <source type="${
+					imageFormat[0].sourceType
+				}" srcset="${imageFormat
+					.map((entry) => entry.srcset)
+					.join(', ')}" sizes="${sizes}">`;
+			})
+			.join('\n')}
+	  <img
+		src="${lowsrc.url}"
+		width="${lowsrc.width}"
+		height="${lowsrc.height}"
+		alt="${alt}"
+		loading="lazy"
+		decoding="async">
+	</picture>`;
+}
 
 module.exports = function(eleventyConfig) {
 	// generate images
